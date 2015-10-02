3b508727bea1c5b9234a8c6b0749e7e17248050c
Talha Javed <talhajaved1994@gmail.com>
2015-10-02 00:31:41 -0400

    Added Infinite Loop

--- Assignment1/prober.py
+++ Assignment1/prober.py
@@ -1,7 +1,24 @@
 #!/usr/bin/python
 
+import sys,httplib,time
+
 hostname = sys.argv[1]
-sample_file = sys.argv[2]
+sample_file_path = sys.argv[2]
 
 print hostname
-print sample_file
+print sample_file_path
+
+sample_file = open(sample_file_path,'w')
+sample_file.write("URL="+hostname)
+
+def executeSomething():
+    current_time = time.time()
+    conn = httplib.HTTPConnection(hostname, 80)
+    conn.request("GET", "/")
+    r1 = conn.getresponse()
+    print r1.status
+    print int(current_time)
+    time.sleep(30)
+
+while True:
+    executeSomething()
