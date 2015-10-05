0f32be81a0b25ceaa46cdd7185d71b51ffe0d962
Talha Javed <talhajaved1994@gmail.com>
2015-10-05 03:37:33 -0400

    Prober output to file added

--- Assignment1/prober.py
+++ Assignment1/prober.py
@@ -9,35 +9,35 @@ print hostname
 print sample_file_path
 
 sample_file = open(sample_file_path,'w')
-sample_file.write("URL="+hostname)
+sample_file.write("URL="+hostname+'\n')
 
-# def executeSomething():
-#     current_time = time.time()
-#     conn = httplib.HTTPConnection(hostname, 80)
-#     conn.request("GET", "/")
-#     r1 = conn.getresponse()
-#     print r1.status
-#     print int(current_time)
-#     time.sleep(30)
+def prober_function():
+    # thread setup to repeat function every 30 seconds
+    t = threading.Timer(30, prober_function)
+    t.daemon = True
+    t.start()
 
-# while True:
-#     executeSomething()
-
-def f():
-    # do something here ...
+    # instructions to execute
+    current_time = time.time()
+    input_line = '' + str(int(current_time)) + ','
     try:
-    	current_time = time.time()
-    	conn = httplib.HTTPConnection(hostname, 80)
+    	conn = httplib.HTTPConnection(hostname, 80, timeout=30)
     	conn.request("GET", "/")
-    	r1 = conn.getresponse()
-    	print r1.status
-    	print int(current_time)
+        r1 = conn.getresponse()
+    	input_line +=  str(r1.status)
     except:
-    	print "error"
+        input_line +=  '-1'
+    sample_file.write(input_line+'\n')
+    sample_file.flush()
+    print "."
+    
 
-
-    # call f() again in 30 seconds
-    threading.Timer(30, f).start()
-
-# start calling f now and every 60 sec thereafter
-f()
+# start calling function now and every 30 sec 
+print "Prober running..."
+prober_function()
+try:
+    while True:
+        time.sleep(1)
+except KeyboardInterrupt:
+    sample_file.close()
+    print ' - Prober Closed'
