7d2009de71cf2dc708f57c990b4f463d052f31ee
Talha Javed <talhajaved1994@gmail.com>
2015-10-05 00:45:56 -0400

    Prober using HTTPlib

--- Assignment1/prober.py
+++ Assignment1/prober.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-import sys,httplib,time
+import sys,httplib,time, threading
 
 hostname = sys.argv[1]
 sample_file_path = sys.argv[2]
@@ -11,14 +11,33 @@ print sample_file_path
 sample_file = open(sample_file_path,'w')
 sample_file.write("URL="+hostname)
 
-def executeSomething():
-    current_time = time.time()
-    conn = httplib.HTTPConnection(hostname, 80)
-    conn.request("GET", "/")
-    r1 = conn.getresponse()
-    print r1.status
-    print int(current_time)
-    time.sleep(30)
+# def executeSomething():
+#     current_time = time.time()
+#     conn = httplib.HTTPConnection(hostname, 80)
+#     conn.request("GET", "/")
+#     r1 = conn.getresponse()
+#     print r1.status
+#     print int(current_time)
+#     time.sleep(30)
 
-while True:
-    executeSomething()
+# while True:
+#     executeSomething()
+
+def f():
+    # do something here ...
+    try:
+    	current_time = time.time()
+    	conn = httplib.HTTPConnection(hostname, 80)
+    	conn.request("GET", "/")
+    	r1 = conn.getresponse()
+    	print r1.status
+    	print int(current_time)
+    except:
+    	print "error"
+
+
+    # call f() again in 30 seconds
+    threading.Timer(30, f).start()
+
+# start calling f now and every 60 sec thereafter
+f()
